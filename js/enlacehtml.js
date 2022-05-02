026f7fcf6da93be37e0c201a2d39d9d6cbc22e53
franz schwartz <gavino_10@hotmail.es>
2022-05-02 13:15:10 -0500

    creando la vista inicio-bienvenida

---
+++ js/enlacehtml.js
@@ -0,0 +1,9 @@
+var menulateral=document.getElementById('menulateral');
+
+$(function(){
+    $(menulateral).load('../bases/menulateral.html');
+});
+
+/* $(function(){
+    $(footer).load('../footer.html');
+}); */
